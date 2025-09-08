e2ce760c3a646c1048c82d1fe407a3e81ca8525f
ChanakaRajapaksha <rcprajapaksha123@gmail.com>
2025-09-09 00:22:04 +0530

    design home page

--- k72-gsap/src/pages/Home.jsx
+++ k72-gsap/src/pages/Home.jsx
@@ -1,9 +1,17 @@
+import HomeBottomText from "../components/home/HomeBottomText";
+import HomeHeroText from "../components/home/HomeHeroText";
 import Video from "../components/home/Video";
 
 const Home = () => {
   return (
-    <div className="h-screen w-screen fixed">
-      <Video />
+    <div className="text-white">
+      <div className="h-screen w-screen fixed">
+        <Video />
+      </div>
+      <div className="h-screen w-screen relative pb-5 overflow-hidden flex flex-col justify-between">
+        <HomeHeroText />
+        <HomeBottomText />
+      </div>
     </div>
   );
 };
